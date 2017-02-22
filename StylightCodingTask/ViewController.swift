a685740a78caf86d2fe0297d538f08cd975dd6f8
Christiane Göhring <christianegohring@christianes-mbp.fritz.box>
2017-02-22 19:46:41 +0100

    Initial Commit

---
+++ StylightCodingTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StylightCodingTask
+//
+//  Created by Christiane Göhring on 22.02.17.
+//  Copyright © 2017 Christiane Göhring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
